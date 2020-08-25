4d4ca9df06ae01040de8538f5f03640df29bb3f1
ChoiJaeEUNG-PC\user <chlwodnd4235@nacer.com>
2020-08-26 00:21:52 +0900

    소스 삭제

--- hmm/src/main/java/com/hmm/hmm/domain/ReplyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hmm.hmm.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
